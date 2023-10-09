a366f89d732d3481c74946a629f16acb8141836b
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 05:50:24 -0700

    Create InvalidScoreEarnedException.java

---
+++ 9-ExceptionHandling-CompileTimeRuntime/InvalidScoreEarnedException.java
@@ -0,0 +1,13 @@
+/**
+ * Exception class for invalid score earned indicator.
+ * @author Tanmay Gupta
+ * @version 1.0, July 16, 2023
+ */
+
+public class InvalidScoreEarnedException extends Exception
+{
+    public InvalidScoreEarnedException(String message)
+    {
+        super(message);
+    }
+}
