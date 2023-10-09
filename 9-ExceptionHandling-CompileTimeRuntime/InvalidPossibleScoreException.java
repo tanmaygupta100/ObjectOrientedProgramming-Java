84947c62823f45ca08044c6184ffbe92e3517739
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 05:50:47 -0700

    Create InvalidPossibleScoreException.java

---
+++ 9-ExceptionHandling-CompileTimeRuntime/InvalidPossibleScoreException.java
@@ -0,0 +1,13 @@
+/**
+ * Exception class for invalid possible score indicator.
+ * @author Tanmay Gupta
+ * @version 1.0, July 16, 2023
+ */
+
+public class InvalidPossibleScoreException extends Exception
+{
+    public InvalidPossibleScoreException(String message)
+    {
+        super(message);
+    }
+}
