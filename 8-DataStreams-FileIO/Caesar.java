194259ca101da95c9a8c2dd9ebb42c4917fc0ee3
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 05:48:05 -0700

    Create Caesar.java

---
+++ 8-DataStreams-FileIO/Caesar.java
@@ -0,0 +1,19 @@
+public class Caesar implements TranslationMatrix
+{
+    private int shift = 3;
+
+    @Override
+    public String translate( String[] words )
+    {
+        StringBuilder translated = new StringBuilder();
+
+        for (String wordsNew : words)
+        {
+            StringBuilder translatedWords = new StringBuilder();
+
+            for
+        }
+
+        return null;
+    }
+}
